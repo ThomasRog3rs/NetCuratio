639f4186b614dd540c4c4359b2f0b392e58157a0
Matt Cunningham <matthewajc@outlook.com>
2021-01-20 21:20:16 +0000

    Add project files.

---
+++ NetCuratio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NetCuratio.Startup))]
+namespace NetCuratio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
