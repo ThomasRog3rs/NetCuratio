93f8614a0bc0ef95650c72d45ebea8f801c504db
Thomas Rogers <42161850+ThomasRog3rs@users.noreply.github.com>
2021-01-23 23:30:59 +0000

    404 error

---
+++ Clients/SySoccer/Dynamic Site/error.php
@@ -0,0 +1,9 @@
+<?php
+    include('includes/navbar.php');
+?>
+<section class="section">
+    <?php include('includes/components/pageNotFoundComponent.php'); ?>
+</section>
+<?php
+    include('includes/footer.php');
+?>
