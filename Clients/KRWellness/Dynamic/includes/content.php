6f42ca19908dd12f10fd87119b11341785136f71
Thomas Rogers <42161850+ThomasRog3rs@users.noreply.github.com>
2021-01-24 21:27:57 +0000

    KRWellness

--- Clients/KRWellness/Dynamic/includes/content.php
+++ Clients/KRWellness/Dynamic/includes/content.php
@@ -3,16 +3,16 @@
     /* This document contains all content for the pages of the site */
 
     // === Site Name ===
-    $siteTitle = "Natours";
+    $siteTitle = "KRWellness";
 
     // === Site Icon ===
     $siteIcon = null;
 
-    $siteLogo = "img/logo-white.png";
+    $siteLogo = "img/kr-logo-white.png";
 
     // === General ===
     $pageBorder = false;
-    $slantedSections = false;
+    $slantedSections = true;
 
     // === Nav Menu Items ===
     $navItems = array(
@@ -25,8 +25,12 @@
             title => "About"
         ),
         array(
+            location => "gallery",
+            title => "Gallery"
+        ),
+        array(
             location => "sessions",
-            title => "Sessions"
+            title => "Services"
         ),
         array(
             location => "testimonials",
@@ -42,60 +46,64 @@
 
     // === Hero Section ===
     /*Note: control the background image in ../css/custom.css */
-    $heroHeading = "Natours";
-    $heroIntro = "Modern Theme";
-    $heroBackgroundImgURL = "https://source.unsplash.com/1600x900/?food";
+    $heroHeading = "KR Wellness";
+    $heroIntro = "Wellness is a choices";
+    $heroBackgroundImgURL = "img/hero-3.jpg";
 
     // hero CTA
     $heroCTA = array(
-        location => "#link",
-        title => "Learn More",
+        location => "sessions",
+        title => "Discover More",
         classNames => "btn btn--white btn--animated"
     );
 
     // === Section One ===
     $sectionOneIcon = null;
-    $sectionOneTitle = "About Us";
+    $sectionOneTitle = "What we are about";
 
-    $sectionOneSubHeading1 = "TERTIARY HEADING";
-    $sectionOneParagraph1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur ducimus quam nisi exercitationem omnis earum qui.";
+    $sectionOneSubHeading1 = "About KR Welness";
+    $sectionOneParagraph1 = "Kieran has over 15 years’ experience within the Health & Fitness managing four Health & Fitness Clubs, 3 of which for Marriott Hotels within the North West, as well running successful Boot Camps, Boxerfit Courses & Fit Clubs for David Lloyd Leisure.";
 
-    $sectionOneSubHeading2 = "TERTIARY HEADING";
-    $sectionOneParagraph2 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur ducimus quam nisi exercitationem omnis earum qui.";
+    $sectionOneSubHeading2 = "Our vision";
+    $sectionOneParagraph2 = "The goal is not only to view health as the physical but to also recognise the mental aspects & how these tackled together can lead to a more sustainable & fulfilled lifestyle on a day to day basis. ";
 
     //Section One CTA
     $sectionOneBtnTitle = null;
     $sectionOneBtnLocation = null;
 
     //3 IMAGE COMPOSITION Component
-    $imageComposition = ["https://source.unsplash.com/1920x1080/?nature","https://source.unsplash.com/1920x1080/?mountain","https://source.unsplash.com/1920x1080/?snow"];
+    $imageComposition = ["img/about1.jpg","img/about2.jpg","img/about3.jpg"];
 
     // Feature cards section
     $infoColumns = array(
         array(
-            icon => "fa-heart",
-            heading => "Heading 1",
-            body => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque ad ea atque fuga."
+            icon => "fa-spa",
+            heading => "Sports Massage",
+            body => "Sports Massage is tailored predominantly to those with injury or problems affecting range of motion
+            which is impacting daily life, such as knee problems and back issues."
         ),
         array(
-            icon => "fa-clock",
-            heading => "Heading 2",
-            body => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque ad ea atque fuga."
+            icon => "fa-dumbbell",
+            heading => "Personal Training",
+            body => "Personal Training is bespoke exercise, tailored to your goals &amp; needs taking in mind your past fitness
+            experience and current fitness levels."
         ),
         array(
-            icon => "fa-user",
-            heading => "Heading 3",
-            body => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque ad ea atque fuga."
+            icon => "fa-seedling",
+            heading => "Nutrition",
+            body => "Diet is such a key part of your everyday life – diet impacts how you feel &amp; how you look, simple as
+            that. I do not believe in fad diets, quick fixes or magic pills."
         ),
         array(
-            icon => "fa-clock",
-            heading => "Heading 2",
-            body => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque ad ea atque fuga."
+            icon => "fa-user-tie",
+            heading => "Corporate Wellness",
+            body => "Corporate Wellness is tailored to your company needs – this can include everything I offer or simply
+            the areas you, as a company, may be focussing on with your staff."
         )
     ); 
 
     // === Review Section ===
-    $reviewSectionTitle = "Client Reviews";
+    $reviewSectionTitle = "We make people genuinely happy";
     $reviewSectionText = "Read the wonderful feedback and success stories we have got from our current and previous clients.";
     $readAllLink = "testimonials";
     $backgroundVideo = false;
@@ -104,11 +112,11 @@
     // === Testimonials ===
     $testimonials = array(
         array(
-            review => "Lorem Ipsum has been the industry's standard since the 1500s. Praesent ullamcorper dui turpis.Nulla non laoreet eleifend.",
+            review => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur ducimus quam nisi exercitationem omnis earum qui. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur ducimus quam nisi exercitationem omnis earum qui.",
             name => "John Doe"
         ),
         array(
-            review => "Lorem Ipsum has been the industry's standard since the 1500s. Praesent ullamcorper dui turpis.Nulla non laoreet eleifend.",
+            review => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur ducimus quam nisi exercitationem omnis earum qui. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur ducimus quam nisi exercitationem omnis earum qui.",
             name => "James Smith"
         ),
         array(
@@ -143,36 +151,65 @@
 
 
     // === Book Section (product / sessions showcase) ===
-    $bookingTitle = "Sessions we offer";
+    $bookingTitle = "Services we offer";
     $learnMorePage = "session?productId=";
     $products = array(
         array(
             productId => 1,
-            productName => "Product Name 1",
-            productPrice => "19.99",
-            productDescription => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur voluptatibus unde iusto dolorem dolor, numquam corporis earum delectus maxime?",
-            productImageURL => "https://source.unsplash.com/1600x900/?session1"
+            productName => "Sports Massage &amp; Deep Tissue",
+            productPrice => "59.99",
+            productDescription => "Sports Massage is tailored predominantly to those with injury or problems affecting range of motion
+            which is impacting daily life, such as knee problems and back issues.<br><br>
+            This type of massage can also be effective in a proactive sense for sports persons &amp; athletes. Regular
+            deep, thorough all over sports massage can be used to keep muscles loose, pliable &amp; relaxed
+            therefor preventing injury.<br><br>
+            I have provided sports massage treatments for both the Manchester United &amp; Manchester City
+            women’s football teams and other levels of all sports.
+            <br><br>
+            Deep Tissue Massage is specifically designed to focus on those tight areas built up from everyday
+            life. Common focus areas include back &amp; shoulder pains &amp; aches and ridding knots.
+            Everyone has focus points that differ to another and this is something we will discuss together prior
+            treatment.
+            <br><br>
+            I have provided massage treatments for a range of reasons on a deep tissue level for people of all
+            shapes and size &amp; most muscular/joint issues you could think of.",
+            productImageURL => "img/contact.jpg"
         ),
         array(
             productId => 2,
-            productName => "Product Name 2",
-            productPrice => "29.99",
-            productDescription => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur voluptatibus unde iusto dolorem dolor, numquam corporis earum delectus maxime?",
-            productImageURL => "https://source.unsplash.com/1600x900/?session2"
+            productName => "Personal Training",
+            productPrice => "59.99",
+            productDescription => "Personal Training is bespoke exercise, tailored to your goals &amp; needs taking in mind your past fitness
+            experience and current fitness levels.<br><br>
+            I look at personal training as a lifestyle piece and really work with you to make a sustainable plan
+            that really fits into your lifestyle in order to maintain. The plan with personal training should never
+            be a temporary solution and I aim to give you the tools to not only achieve your goals but to
+            maintain them and evolve goals as you progress.",
+            productImageURL => "https://source.unsplash.com/1600x900/?gym"
         ),
         array(
             productId => 3,
-            productName => "Product Name 3",
+            productName => "Nutrition",
             productPrice => "39.99",
-            productDescription => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur voluptatibus unde iusto dolorem dolor, numquam corporis earum delectus maxime?",
-            productImageURL => "https://source.unsplash.com/1600x900/?session3"
+            productDescription => "Diet is such a key part of your everyday life – diet impacts how you feel &amp; how you look, simple as
+            that. I do not believe in fad diets, quick fixes or magic pills.<br><br>
+            Food diaries are a great way for me to gain a greater understanding of your diet as well as lifestyle
+            given the detail around the times you are eating. By making gradual adjustments to your own diet
+            we can work together to work toward a balanced diet that you will stick to.<br><br>
+            This will not be ‘A Diet’, it will be your diet!",
+            productImageURL => "https://source.unsplash.com/1600x900/?food"
         ),
         array(
             productId => 4,
-            productName => "Product Name 4",
-            productPrice => "39.99",
-            productDescription => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur voluptatibus unde iusto dolorem dolor, numquam corporis earum delectus maxime?",
-            productImageURL => "https://source.unsplash.com/1600x900/?session4"
+            productName => "Corp Wellness",
+            productPrice => "299.99",
+            productDescription => "Corporate Wellness is tailored to your company needs – this can include everything I offer or simply
+            the areas you, as a company, may be focussing on with your staff. This is key in staff retention &amp;
+            productivity. A happier workforce is a healthy workforce.<br><br>
+            Group training is always a good place to start, especially for moral boosting &amp; team building.<br><br>
+            Speak with me to discuss the needs of your business &amp; we’ll work toward a bespoke wellness
+            package to compliment your business.",
+            productImageURL => "https://source.unsplash.com/1600x900/?business"
         )
     ); 
     $sessionsBtnTitle = "View All Sessions &rarr;";
@@ -244,10 +281,10 @@
     );
 
     // === Contact Section ===
-    $contactBackgroundImgURL = "https://source.unsplash.com/1600x900/?nature";
+    $contactBackgroundImgURL = "img/contact.jpg";
 
     // === Footer Links ===
     $footerLinks = $navItems;
-    $footerCopyRight = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis lectus eget ex scelerisque, ut rhoncus sapien fringilla. Nulla purus purus, tristique at sollicitudin id, mattis vitae lectus. Etiam ultrices elit ac eros cursus commodo. Quisque condimentum, ligula sed finibus.";
+    $footerCopyRight = "KR_Welness 2021 | Powered by NetCuratio";
 
 ?>
--- Clients/KRWellness/Dynamic/includes/sections/sectionFeatures.php
+++ Clients/KRWellness/Dynamic/includes/sections/sectionFeatures.php
@@ -3,7 +3,7 @@
         <?php foreach($infoColumns as $featureBox) : ?>
             <div class="col-1-of-4">
                 <div class="feature-box">
-                    <i class="feature-box__icon far <?php echo $featureBox[icon] ?> fa-5x"></i>
+                    <i class="feature-box__icon fas <?php echo $featureBox[icon] ?> fa-5x"></i>
                     <h3 class="heading-tertiary u-margin-bottom-small"><?php echo $featureBox[title] ?></h3>
                     <p class="feature-box__text">
                         <?php echo $featureBox[body] ?>
