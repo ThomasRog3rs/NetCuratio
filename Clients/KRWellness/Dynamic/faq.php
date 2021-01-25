e8a4d644a134beb5425cb71e281aa0613fb478ec
Thomas Rogers <42161850+ThomasRog3rs@users.noreply.github.com>
2021-01-25 11:56:09 +0000

    faq and session updates

---
+++ Clients/KRWellness/Dynamic/faq.php
@@ -0,0 +1,20 @@
+<?php
+    include('includes/navbar.php');
+?>
+<div class="header__text-box">
+    <h1 class="heading-primary" style="margin: auto 0px">
+        <span class="heading-primary--sub">FAQ</span>
+    </h1>
+</div>
+</header>
+
+<main>
+
+    <section class="section">
+        <div class="row">
+            <?php include('includes/components/faqComponent.php'); ?>
+        </div>
+    </section>
+<?php
+    include('includes/footer.php');
+?>
