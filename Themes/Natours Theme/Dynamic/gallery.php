1047bdc9506465e8ca0a24d1a832ea7afd46b6c6
Thomas Rogers <42161850+ThomasRog3rs@users.noreply.github.com>
2021-01-25 12:37:39 +0000

    contact form altert-success

--- Themes/Natours Theme/Dynamic/gallery.php
+++ Themes/Natours Theme/Dynamic/gallery.php
@@ -3,18 +3,18 @@
 ?>
 <div class="header__text-box">
     <h1 class="heading-primary" style="margin: auto 0px">
-        <span class="heading-primary--sub">Contact Us</span>
+        <span class="heading-primary--sub">Gallery</span>
     </h1>
 </div>
 </header>
 
 <main>
+
     <section class="section">
         <div class="row">
-            <?php include('includes/components/contactComponent.php'); ?>
+            <div class="elfsight-app-b8cfb078-34d5-4612-a44b-ca21179d4b5b"></div>
         </div>
     </section>
-
 <?php
     include('includes/footer.php');
 ?>
