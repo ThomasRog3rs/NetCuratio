0bf80fc32bfd68356a79556258d23d046b45a003
Thomas Rogers <42161850+ThomasRog3rs@users.noreply.github.com>
2021-01-25 11:28:49 +0000

    added header.php file

--- Clients/KRWellness/Dynamic/includes/navbar.php
+++ Clients/KRWellness/Dynamic/includes/navbar.php
@@ -1,32 +1,4 @@
-<?php include('includes/content.php'); ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
-
-        <!--<link rel="stylesheet" href="css/icon-font.css">-->
-        
-
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
-            integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous" />
-
-        <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="css/custom.css">
-        <?php if(!$pageBorder) : ?>
-            <link rel="stylesheet" href="css/removeBorder.css">
-        <?php endif; ?>
-        <?php if(!$slantedSections) : ?>
-            <link rel="stylesheet" href="css/removeSlantedSections.css">
-        <?php endif; ?>
-        
-        <link rel="shortcut icon" type="image/png" href="img/favicon.png">
-        
-        <title>Natours Theme</title>
-    </head>
+<?php include('includes/header.php'); ?>
 
     <body>
         <div class="navigation">
