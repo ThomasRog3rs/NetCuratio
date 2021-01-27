56422500958428a3e3f6566f806585be380c0b2b
Matt Cunningham <matthewajc@outlook.com>
2021-01-27 19:43:26 +0000

    NetCuratio site initial commit

---
+++ NetCuratioSite/NetCuratio/NetCuratio/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetCuratio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.CatchPhrase = "Beautiful websites, expertly made.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.CatchPhrase = "Beautiful websites, expertly made.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.CatchPhrase = "Beautiful websites, expertly made.";
+
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            ViewBag.CatchPhrase = "Beautiful websites, expertly made.";
+
+            return View();
+        }
+
+        //public ActionResult Blog()
+        //{
+        //    ViewBag.CatchPhrase = "Beautiful websites, expertly made.";
+
+        //    return View();
+        //}
+    }
+}
---
+++ NetCuratioSite/NetCuratio/NetCuratio/Services/EmailService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace NetCuratio.Services
+{
+    public class EmailService
+    {
+        public void SendEmail()
+        {
+            try
+            {
+
+                SmtpClient mySmtpClient = new SmtpClient("my.smtp.exampleserver.net");
+
+                // set smtp-client with basicAuthentication
+                mySmtpClient.UseDefaultCredentials = false;
+                System.Net.NetworkCredential basicAuthenticationInfo = new
+                System.Net.NetworkCredential("username", "password");
+                mySmtpClient.Credentials = basicAuthenticationInfo;
+
+                // add from,to mailaddresses
+                MailAddress from = new MailAddress("test@example.com", "TestFromName");
+                MailAddress to = new MailAddress("matthewajc@outlook.com", "TestToName");
+                MailMessage myMail = new System.Net.Mail.MailMessage(from, to);
+
+                // add ReplyTo
+                MailAddress replyTo = new MailAddress("reply@example.com");
+                myMail.ReplyToList.Add(replyTo);
+
+                // set subject and encoding
+                myMail.Subject = "Test message";
+                myMail.SubjectEncoding = System.Text.Encoding.UTF8;
+
+                // set body-message and encoding
+                myMail.Body = "<b>Test Mail</b><br>using <b>HTML</b>.";
+                myMail.BodyEncoding = System.Text.Encoding.UTF8;
+                // text or html
+                myMail.IsBodyHtml = true;
+
+                mySmtpClient.Send(myMail);
+            }
+
+            catch (SmtpException ex)
+            {
+                throw new ApplicationException
+                  ("SmtpException has occured: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
