5c06581d96a4a31bdb44aac48b4d64ac9d34a1c1
Thomas Rogers <42161850+ThomasRog3rs@users.noreply.github.com>
2021-01-27 16:15:11 +0000

    Trunc function

--- Themes/Natours Theme/Dynamic/includes/content.php
+++ Themes/Natours Theme/Dynamic/includes/content.php
@@ -1,4 +1,12 @@
 <?php 
+
+    function trunc($phrase, $max_words) {
+        $phrase_array = explode(' ',$phrase);
+        if(count($phrase_array) > $max_words && $max_words > 0)
+        $phrase = implode(' ',array_slice($phrase_array, 0, $max_words)).'...';
+        return $phrase;
+    }
+    
     $websiteEmail = "thomas@trwebdev.com";
     /* This document contains all content for the pages of the site */
 
