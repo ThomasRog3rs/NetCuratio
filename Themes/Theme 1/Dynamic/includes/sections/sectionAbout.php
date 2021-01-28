dd188c48dc125d7aa59860cbe6e69184693fecd4
Thomas Rogers <42161850+ThomasRog3rs@users.noreply.github.com>
2021-01-28 00:27:36 +0000

    Update sectionAbout.php

--- Themes/Theme 1/Dynamic/includes/sections/sectionAbout.php
+++ Themes/Theme 1/Dynamic/includes/sections/sectionAbout.php
@@ -2,11 +2,6 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
-                <?php 
-                    // include('includes/components/youtubeVideoComponent.php');
-                    // $video =  new YouTubeVideo();
-                    // $video->displayVideo("https://www.youtube.com/embed/5jyVjdkaAsg");
-                ?>
                 <div class="text-center mb-4">
                     <?php if($aboutSectionProfile != null) : ?>
                         <img src="<?php echo $aboutSectionProfile ?>"
@@ -14,6 +9,11 @@
                     <?php endif; ?>
                 </div>
                 <h2 class="text-center mb-5"><?php echo $aboutSectionTitle ?></h2>
+                <?php 
+                    // include('includes/components/youtubeVideoComponent.php');
+                    // $video =  new YouTubeVideo();
+                    // $video->displayVideo("https://www.youtube.com/embed/5jyVjdkaAsg");
+                ?>
                 <p class="sub-text text-center mb-5"><?php echo $aboutSectionText ?></p>
             </div>
         </div>
