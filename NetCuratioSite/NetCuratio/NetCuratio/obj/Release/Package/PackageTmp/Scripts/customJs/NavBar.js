fc148afceef4f4c3b0a8328f6da27f959f20d343
Matt Cunningham <matthewajc@outlook.com>
2021-03-21 00:49:45 +0000

    Navbar IOS fix.

--- NetCuratioSite/NetCuratio/NetCuratio/obj/Release/Package/PackageTmp/Scripts/customJs/NavBar.js
+++ NetCuratioSite/NetCuratio/NetCuratio/obj/Release/Package/PackageTmp/Scripts/customJs/NavBar.js
@@ -1,5 +1,5 @@
 ﻿//Variables required
-const navBar = document.querySelector("nav.header-section");
+const navBar = document.querySelector("nav.header-section-secondary");
 const navLinks = document.querySelectorAll("a.main-menu-link");
 let prevScrollpos = window.pageYOffset;
 
@@ -11,11 +11,14 @@ window.onscroll = () => {
     //Check if the user is scrolling up
     if (prevScrollpos > currentScrollPos) {
         //show the navbar
+        navBar.style.opacity = "1";
+        navBar.style.pointerEvents = "all";
+        navBar.style.position = "fixed";
         navBar.style.top = "0";
         navBar.classList += " shadow-lg";
         navBar.style.background = "rgba(33, 0, 79, 0.97)";
         navBar.style.background = "#1b043b";
-        navBar.style.position = "fixed";
+        navBar.style.opacity = "0.95";
 
         try {
             navBar.classList.remove("test");
@@ -33,10 +36,8 @@ window.onscroll = () => {
 
     //If user is at the top of the page
     if (prevScrollpos == 0) {
-        navBar.style.background = "transparent";
-        navBar.classList.remove("shadow-lg");
-        navBar.classList += (" test");
-        //navBar.style.position = "absolute";
+        navBar.style.opacity = "0";
+        navBar.style.pointerEvents = "none";
     }
 
 }
